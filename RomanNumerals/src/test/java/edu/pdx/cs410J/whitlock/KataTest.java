64fb890793a7c06b4cf6a96d562fcb2144e59bf4
David Whitlock <david.m.whitlock@gmail.com>
2020-07-08 20:46:06 -0700

    Looks like the recursive algorithm has potential.

--- RomanNumerals/src/test/java/edu/pdx/cs410J/whitlock/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/whitlock/KataTest.java
@@ -61,6 +61,29 @@ public class KataTest
   @Test
   public void romanNumeralsBetween11And20() {
     assertThat(Kata.romanNumeralFor(11), equalTo("XI"));
+    assertThat(Kata.romanNumeralFor(12), equalTo("XII"));
+    assertThat(Kata.romanNumeralFor(13), equalTo("XIII"));
+    assertThat(Kata.romanNumeralFor(14), equalTo("XIV"));
+    assertThat(Kata.romanNumeralFor(15), equalTo("XV"));
+    assertThat(Kata.romanNumeralFor(16), equalTo("XVI"));
+    assertThat(Kata.romanNumeralFor(17), equalTo("XVII"));
+    assertThat(Kata.romanNumeralFor(18), equalTo("XVIII"));
+    assertThat(Kata.romanNumeralFor(19), equalTo("XIX"));
+    assertThat(Kata.romanNumeralFor(20), equalTo("XX"));
+  }
+
+  @Test
+  public void romanNumeralsBetween21And30() {
+    assertThat(Kata.romanNumeralFor(21), equalTo("XXI"));
+    assertThat(Kata.romanNumeralFor(22), equalTo("XXII"));
+    assertThat(Kata.romanNumeralFor(23), equalTo("XXIII"));
+    assertThat(Kata.romanNumeralFor(24), equalTo("XXIV"));
+    assertThat(Kata.romanNumeralFor(25), equalTo("XXV"));
+    assertThat(Kata.romanNumeralFor(26), equalTo("XXVI"));
+    assertThat(Kata.romanNumeralFor(27), equalTo("XXVII"));
+    assertThat(Kata.romanNumeralFor(28), equalTo("XXVIII"));
+    assertThat(Kata.romanNumeralFor(29), equalTo("XXIX"));
+    assertThat(Kata.romanNumeralFor(30), equalTo("XXX"));
   }
 
 }
