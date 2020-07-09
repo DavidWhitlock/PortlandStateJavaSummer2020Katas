9dd0eff90cdfd939e99bb1050488dcf9ed6e6b79
David Whitlock <david.m.whitlock@gmail.com>
2020-07-08 20:43:37 -0700

    Implement more conversion of numbers to roman numerals.

--- RomanNumerals/src/main/java/edu/pdx/cs410J/whitlock/Kata.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/whitlock/Kata.java
@@ -2,15 +2,61 @@ package edu.pdx.cs410J.whitlock;
 
 /**
  * A class for getting started with a code kata
- *
+ * <p>
  * Use IntelliJ's "Refactor | Rename..." command to change the name of this
  * class (and its tests).
  */
 public class Kata {
-                                                                                    
+
 
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
     System.exit(1);
+  }
+
+  static String romanNumeralFor(int number) {
+    if (number > 10) {
+      return "X" + romanNumeralFor(number - 10);
+
+    } else {
+      return romanNumeralBetween1And10(number);
+    }
+  }
+
+  private static String romanNumeralBetween1And10(int number) {
+    switch (number) {
+      case 1:
+        return "I";
+
+      case 2:
+        return "II";
+
+      case 3:
+        return "III";
+
+      case 4:
+        return "IV";
+
+      case 5:
+        return "V";
+
+      case 6:
+        return "VI";
+
+      case 7:
+        return "VII";
+
+      case 8:
+        return "VIII";
+
+      case 9:
+        return "IX";
+
+      case 10:
+        return "X";
+
+      default:
+        return null;
+    }
   }
 }
--- RomanNumerals/src/test/java/edu/pdx/cs410J/whitlock/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/whitlock/KataTest.java
@@ -2,12 +2,65 @@ package edu.pdx.cs410J.whitlock;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class KataTest
 {
 
   @Test
-  public void canInstantiateKataClass() {
-    new Kata();
+  public void romanNumeralFor1IsI() {
+    assertThat(Kata.romanNumeralFor(1), equalTo("I"));
+  }
+
+  @Test
+  public void romanNumeralFor2IsII() {
+    assertThat(Kata.romanNumeralFor(2), equalTo("II"));
+  }
+
+  @Test
+  public void romanNumeralFor3IsIII() {
+    assertThat(Kata.romanNumeralFor(3), equalTo("III"));
+  }
+
+  @Test
+  public void romanNumeralFor4IsIV() {
+    assertThat(Kata.romanNumeralFor(4), equalTo("IV"));
+  }
+
+  @Test
+  public void romanNumeralFor5IsV() {
+    assertThat(Kata.romanNumeralFor(5), equalTo("V"));
+  }
+
+  @Test
+  public void romanNumeralFor6IsVI() {
+    assertThat(Kata.romanNumeralFor(6), equalTo("VI"));
+  }
+
+  @Test
+  public void romanNumeralFor7IsVII() {
+    assertThat(Kata.romanNumeralFor(7), equalTo("VII"));
+  }
+
+  @Test
+  public void romanNumeralFor8IsVIII() {
+    assertThat(Kata.romanNumeralFor(8), equalTo("VIII"));
+  }
+
+  @Test
+  public void romanNumeralFor9IsIX() {
+    assertThat(Kata.romanNumeralFor(9), equalTo("IX"));
+  }
+
+  @Test
+  public void romanNumeralFor10IsX() {
+    assertThat(Kata.romanNumeralFor(10), equalTo("X"));
+  }
+
+  @Test
+  public void romanNumeralsBetween11And20() {
+    assertThat(Kata.romanNumeralFor(11), equalTo("XI"));
   }
 
 }
